d4e49ccea71ab1831cb496eefd2982583288e2e3
Jhon Jaider Echavarria Tapias <jaiderechavarria10@gmail.com>
2022-11-18 15:42:44 -0500

    Back casi completo

--- blueprints/reports_blueprints.py
+++ blueprints/reports_blueprints.py
@@ -2,10 +2,27 @@ from flask import Blueprint
 from controllers.reports_controller import ReportController
 
 report_blueprints = Blueprint('report_blueprints', __name__)
-report_controller = ReportController()
+reports_controller = ReportController()
 
 
 @report_blueprints.route("/reports/votes_by_candidate", methods=['GET'])
-def votes_by_candidate():
-    response = report_controller.get_votes()
+def get_votes_by_candidate():
+    response = reports_controller.get_votes_by_candidate()
     return response, 200
+
+
+@report_blueprints.route("/reports/candidates_by_tables", methods=['GET'])
+def get_votes_by_table():
+    response = reports_controller.get_votes_by_table()
+    return response, 200
+
+
+@report_blueprints.route("/reports/votes_by_political_party", methods=['GET'])
+def get_votes_by_political_party():
+    pass
+
+
+@report_blueprints.route("/reports/percentage votes_parties", methods=['GET'])
+def get_political_party_percentage_votes():
+    pass
+
--- controllers/reports_controller.py
+++ controllers/reports_controller.py
@@ -5,5 +5,8 @@ class ReportController:
     def __init__(self):
         self.report_repository = ReportRepository()
 
-    def get_votes(self):
+    def get_votes_by_candidate(self):
         return self.report_repository.get_votes_by_candidate()
+
+    def get_votes_by_table(self):
+        return self.report_repository.get_votes_by_table()
